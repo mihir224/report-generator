c9593349d3efd6aa83d32f068a90ecf2fb300dad
mihir224 <mihirsaini25@gmail.com>
2023-02-11 19:20:02 +0530

    functionality to parse .csv files

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import axios from 'axios';
 import {saveAs} from 'file-saver';
 import './App.css';
+import Papa from  'papaparse';
 
 function App() {
    const [data,setData]=React.useState({
@@ -12,29 +13,111 @@ function App() {
     Cash:0,
     Digital:0
   });
-  function handleChange({target:{value,name}}){ //de-structuring synthetic event object to use only the target object
-    setData({
-      ...data,
-      [name]:value
-    });
-  }
-  function handleClick(){
-    axios.post("/generate-pdf",data)
-      .then(()=> axios.get('/fetch-report',{responseType:'blob'}))
-      .then((res)=>{
-        const reportBlob=new Blob([res.data],{type:'application/pdf'})
-        saveAs(reportBlob,'report.pdf');
+   const [col,setCol]=React.useState([]);
+   const [values,setValues]=React.useState([]);
+  // function handleChange({target:{value,name}}){ //de-structuring synthetic event object to use only the target object
+  //   setData({
+  //     ...data,
+  //     [name]:value
+  //   });
+  // }
+
+
+    function handleChange(event){
+      Papa.parse(event.target.files[0],{
+        header:true,
+        skipEmptyLines:true,
+        complete:function(result){
+          // console.log(result.data);
+          const columnArray=[];
+          const valuesArray=[];
+          result.data.map((d)=>{
+            columnArray.push(Object.keys(d));
+            valuesArray.push(Object.values(d));
+          });
+          setData(result.data);
+          setCol(columnArray[0]);
+          setValues(valuesArray);
+        }
       })
     }
+
+    React.useEffect(()=>{
+      var countBike=0;
+      var countCar=0;
+      var countAuto=0;
+      var countCash=0;
+      var countDigital=0;
+     for(var i=0;i<values.length;i++){
+       for(var j=0;j<values[i].length;j++){
+         if(values[i][j]==='Bike'){
+           col.map((val,index)=>{
+             if(val==='Pricing'){
+              countBike+=parseFloat(values[i][index]);
+             }
+           })
+           
+         }
+         else if(values[i][j]==='Car'){
+          col.map((val,index)=>{
+            if(val==='Pricing'){
+             countCar+=parseFloat(values[i][index]);
+            }
+          })
+         }
+         else if(values[i][j]==='Auto'){
+          col.map((val,index)=>{
+            if(val==='Pricing'){
+             countAuto+=parseFloat(values[i][index]);
+            }
+          })
+         }
+         else if(values[i][j]==='CashPayment'){
+          col.map((val,index)=>{
+            if(val==='Total'){
+             countCash+=parseFloat(values[i][index]);
+            }
+          })
+         }
+         else if(values[i][j]==='DigitalPayment'){
+          col.map((val,index)=>{
+            if(val==='Total'){
+             countDigital+=parseFloat(values[i][index]);
+            }
+          })
+         }
+       }
+     }
+     const unpaid=parseFloat((countCash)+(countDigital))-((countBike)+(countAuto)+(countCar));
+        setData({
+          Bikes:countBike.toFixed(2),
+          Autos:countAuto.toFixed(2),
+          Cars:countCar.toFixed(2),
+          Unpaid:unpaid.toFixed(2),
+          Cash:countCash.toFixed(2),
+          Digital:countDigital.toFixed(2)
+        });
+    },[col,values]);
+    
+    function handleClick(){
+      axios.post("/generate-pdf",data)
+        .then(()=> axios.get('/fetch-report',{responseType:'blob'}))
+        .then((res)=>{
+          const reportBlob=new Blob([res.data],{type:'application/pdf'})
+          saveAs(reportBlob,'report.pdf');
+        })
+      }
   return (
     <div className="App">
-     <input type="number" placeholder="Bikes" name="Bikes" onChange={handleChange}></input>
+     {/* <input type="number" placeholder="Bikes" name="Bikes" onChange={handleChange}></input>
      <input type="number" placeholder="Autos" name="Autos" onChange={handleChange}></input>
      <input type="number" placeholder="Cars" name="Cars" onChange={handleChange}></input> 
      <input type="number" placeholder="Unpaid Amount" name="Unpaid" onChange={handleChange}></input>
      <h3>Collections from different payment modes</h3>
      <input type="number" placeholder="Cash" name="Cash" onChange={handleChange}></input>
      <input type="number" placeholder="Digital" name="Digital" onChange={handleChange}></input>
+      */}
+     <input type="file" name="file" accept=".csv" onChange={handleChange}/>
      <button onClick={handleClick}>Generate PDF</button>
     </div>
   );
